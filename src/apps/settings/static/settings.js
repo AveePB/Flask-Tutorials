68abe8c1ca002e88fd182b1d4d3fd6d12780f39c
AveePB <mike303.py@gmail.com>
2024-09-11 18:43:07 +0200

    Completed delete bio

--- src/apps/settings/static/settings.js
+++ src/apps/settings/static/settings.js
@@ -77,4 +77,62 @@ document.addEventListener('DOMContentLoaded', function() {
     } else {
         console.error('Toggle password button or password field not found');
     }
+
+    // Delete buttons
+    const deleteButtons = document.querySelectorAll('.delete-btn');
+
+    deleteButtons.forEach(deleteButton => {
+        deleteButton.addEventListener('click', function(event) {
+            event.preventDefault();
+
+            // Retrieve the data attributes (URL, item ID, and message box ID) from the button
+            const url = deleteButton.getAttribute('data-url');
+            const messageBoxId = deleteButton.getAttribute('data-message-box');
+            const messageBox = document.getElementById(messageBoxId);
+
+            // Confirmation dialog before deletion
+            if (confirm(`Are you sure you want to delete it?`)) {
+                fetch(url, {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    }
+                })
+                .then(response => response.text().then(text => {
+                    let data = {};
+                    try {
+                        data = JSON.parse(text);
+                    } catch {
+                        data.message = text;
+                    }
+                    return { status: response.status, message: data.message };
+                }))
+                .then(result => {
+                    // Clear previous classes
+                    messageBox.classList.remove('success', 'error');
+
+                    // Display the result
+                    messageBox.style.display = 'block';
+                    if (result.status === 204 || result.status === 200) {
+                        messageBox.classList.add('success');
+                        messageBox.innerHTML = result.message || `Successfully deleted.`;
+                        setTimeout(() => {
+                            window.location.reload();
+                        }, 500);
+                    } else {
+                        messageBox.classList.add('error');
+                        messageBox.innerHTML = result.message || `Failed to delete.`;
+                    }
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                    messageBox.classList.remove('success');
+                    messageBox.classList.add('error');
+                    messageBox.style.display = 'block';
+                    messageBox.innerHTML = 'An error occurred. Please try again.';
+                });
+            }
+        });
+    });
+
 });
